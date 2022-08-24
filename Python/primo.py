8157c5e75cf803b5e1e3aa79fc01324c9bd3e679
Guillermo Dulce <93414316+guillermodulce@users.noreply.github.com>
2022-08-24 14:23:09 -0300

    Create primo.py

---
+++ Python/primo.py
@@ -0,0 +1,17 @@
+num=int(input("Ingrese un numero: "))
+
+if num > 1:
+    cont=0
+    i=2
+    while i<num and cont==0:
+        resto=num%i
+        
+        if resto==0:
+            cont+=1
+        i+=1
+    if cont==0:
+        print(f"El {num} es un número primo")
+    else:
+        print(f"El {num} no es un número primo")
+else:
+    print(f"El {num} no es un número primo")
