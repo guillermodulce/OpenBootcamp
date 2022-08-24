3e6160201a57d22e5060504d65e32ae9b0bdc166
Guillermo Dulce <93414316+guillermodulce@users.noreply.github.com>
2022-08-23 21:32:52 -0300

    Update area.py

--- Python/area.py
+++ Python/area.py
@@ -5,7 +5,7 @@ def area_triangulo(altura, base):
 a = int(input('Ingrese la altura del triangulo: '))
 b = int(input('Ingrese la base del triangulo: '))
 
-def area_circulo(radio)
+def area_circulo(radio):
     return (3.141592653*(radio**2))
     
 r = int(input('Ingrese el radio del circulo: '))
