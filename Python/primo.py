9a30871b954880ebc92c05bbd6a8c228b2fd3cb2
Guillermo Dulce <93414316+guillermodulce@users.noreply.github.com>
2022-08-24 15:06:48 -0300

    Update primo.py

--- Python/primo.py
+++ Python/primo.py
@@ -1,17 +1,18 @@
-num=int(input("Ingrese un numero: "))
+def numprimo():
+    num=int(input("Ingrese un numero: "))
 
-if num > 1:
-    cont=0
-    i=2
-    while i<num and cont==0:
-        resto=num%i
-        
-        if resto==0:
-            cont+=1
-        i+=1
-    if cont==0:
-        print(f"El {num} es un número primo")
-    else:
-        print(f"El {num} no es un número primo")
-else:
-    print(f"El {num} no es un número primo")
+    if num > 1:
+        cont=0
+        i=2
+        while i<num and cont==0:
+            resto=num%i
+            
+            if resto==0:
+                cont+=1
+            i+=1
+        if cont==0:
+            return f"El {num} es un número primo"
+        else:
+            return f"El {num} no es un número primo"
+    
+print(numprimo())
