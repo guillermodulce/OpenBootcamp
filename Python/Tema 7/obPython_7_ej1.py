b651b1e515b98693828181a02a57a32ec6c0f1d7
Guillermo Dulce <guillermodulce@gmail.com>
2022-08-26 20:14:05 -0300

    vscode

--- Python/Tema 7/obPython_7_ej1.py
+++ Python/Tema 7/obPython_7_ej1.py
@@ -1,4 +1,3 @@
-from distutils.command.clean import clean
 from calculadora import *
 
 a, b = (10, 4)
@@ -7,4 +6,3 @@ print( "{} + {} = {}".format(a, b, suma(a, b) ) )
 print( "{} - {} = {}".format(a, b, resta(a, b) ) )
 print( "{} * {} = {}".format(a, b, multiplicación(a, b) ) ) 
 print( "{} / {} = {}".format(a, b, división(a, b) ) )
-clean
