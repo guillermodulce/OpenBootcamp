17a7645044a0632bd34361d6f39dd556631b2b3f
Guillermo Dulce <93414316+guillermodulce@users.noreply.github.com>
2022-09-07 18:18:37 -0300

    Delete Python/Django directory

--- Python/Django/OB_Django/Directores/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Director, Actor, Movie
-
-admin.site.register(Director)
-admin.site.register(Actor)
-admin.site.register(Movie)
