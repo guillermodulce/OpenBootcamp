c39448215823ac01e34e3367e5c26b463f6c81c0
Guillermo Dulce <guillermodulce@gmail.com>
2022-09-06 23:09:11 -0300

    add db movies

--- Python/Django/OB_Django/Directores/admin.py
+++ Python/Django/OB_Django/Directores/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Directores, Actores, Peliculas
+
+admin.site.register(Directores)
+admin.site.register(Actores)
+admin.site.register(Peliculas)
--- Python/Django/OB_Django/Directores/models.py
+++ Python/Django/OB_Django/Directores/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Directores(models.Model):
+    name = models.CharField(max_length=64, help_text="Nombre del director")
+    country = models.CharField(max_length=64, null=True, help_text="Pais de origen del director")
+    born_date = models.CharField(max_length=4, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Actores(models.Model):
+    actor_name = models.CharField(max_length=64, help_text="Nombre del actor o actriz")
+
+    def __str__(self):
+        return self.actor_name
+
+class Peliculas(models.Model):
+    title = models.CharField(max_length=50)
+    year = models.CharField(max_length=4, null=True)
+    director = models.ForeignKey('directores', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='Descripción de la pelicula')
+    performer = models.ManyToManyField(Actores)
+
+def __str__(self):
+    return self.title
+
+
+
+
