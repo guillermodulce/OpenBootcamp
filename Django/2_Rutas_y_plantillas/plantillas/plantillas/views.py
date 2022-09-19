e17eb98924e902966eb2a1ad85bc695207086e3a
Guillermo Dulce <guillermodulce@gmail.com>
2022-09-19 19:47:15 -0300

    add dinamico

--- Django/2_Rutas_y_plantillas/plantillas/plantillas/views.py
+++ Django/2_Rutas_y_plantillas/plantillas/plantillas/views.py
@@ -1,4 +1,11 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 def simple(request):
     return render(request, 'simple.html', {})
+
+def dinamico(request, name):
+    categories = ['code', 'design', 'marketing']
+    context = {'name' : name, 'categories' : categories}
+    return render(request, 'dinamico.html', context)
+
