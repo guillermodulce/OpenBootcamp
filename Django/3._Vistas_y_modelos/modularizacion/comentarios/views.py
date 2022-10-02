a0d72e81febb476eb782223f31e104e6597e1b61
Guillermo Dulce <guillermodulce@gmail.com>
2022-10-01 21:31:49 -0300

    creación y borrado de datos

--- Django/3._Vistas_y_modelos/modularizacion/comentarios/views.py
+++ Django/3._Vistas_y_modelos/modularizacion/comentarios/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Comment
 
 def test(request):
     return HttpResponse("Funciona correctamente")
 
 def create(request):
+    #comment = Comment(name="Guille", score=9, comment="Este es un comentario")
+    #comment.save()
+    comment = Comment.objects.create(name="Valentino")
     return HttpResponse("Ruta para probar la creacion de modelos")
+
+def delete(request):
+    #comment = Comment.objects.get(id=1)
+    #comment.delete()
+    Comment.objects.filter(id=2).delete()
+    return HttpResponse("Ruta para probar los borrados")
