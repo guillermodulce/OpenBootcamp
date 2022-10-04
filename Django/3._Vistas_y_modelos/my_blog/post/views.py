4af57efb57fc42f6238a9b4af4a98c4516e55589
Guillermo Dulce <guillermodulce@gmail.com>
2022-10-04 00:41:55 -0300

    Consultas de datos 2

--- Django/3._Vistas_y_modelos/my_blog/post/views.py
+++ Django/3._Vistas_y_modelos/my_blog/post/views.py
@@ -7,7 +7,7 @@ def queries(request):
     authors = Author.objects.all()
 
     # Obtener datos filtrados por condición
-    filtered = Author.objects.filter(email='rachel60@example.org')
+    # filtered = Author.objects.filter(email='rachel60@example.org')
 
     # Obtener un único objeto (filtrado)
     author = Author.objects.get(id=1)
@@ -18,5 +18,16 @@ def queries(request):
     # Obtener 5 elementos saltando los 5 primeros
     offsets = Author.objects.all()[5:10]
 
-    return render(request, 'post/queries.html', {'authors': authors, 'filtered': filtered, 'author': author, 'limits': limits, 'offsets': offsets})
+    # Obtener todos los elementos ordenados
+    orders = Author.objects.all().order_by('email')
+
+    # Obtener todos los elementos cuyo id sea menor o igual a 15
+    filtered = Author.objects.filter(id__lte=15)
+
+    # Obtener todos los autores que contienen en su nombre la palabra yes
+    # filtered = Author.objects.filter(name__contains="yes")
+
+
+
+    return render(request, 'post/queries.html', {'authors': authors, 'filtered': filtered, 'author': author, 'limits': limits, 'offsets': offsets, 'orders': orders})
 
