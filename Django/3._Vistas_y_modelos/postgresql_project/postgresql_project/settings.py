31958b6f7da93122aa318469ff3aba23d0560caa
Guillermo Dulce <guillermodulce@gmail.com>
2022-10-15 23:26:29 -0300

    remove pass postgresql

--- Django/3._Vistas_y_modelos/postgresql_project/postgresql_project/settings.py
+++ Django/3._Vistas_y_modelos/postgresql_project/postgresql_project/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_sample',
         'USER': 'postgres',
-        'PASSWORD': 'sqlAdminG',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
