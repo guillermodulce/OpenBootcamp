e2c867a2fdddb814772d35359aae4daeec1f352b
Guillermo Dulce <guillermodulce@gmail.com>
2022-10-24 12:49:41 -0300

    Formularios a través de clases

--- Django/4_Formularios/formularios_django/formularios_django/views.py
+++ Django/4_Formularios/formularios_django/formularios_django/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import CommentForm
 
 def form(request):
-    return render(request, 'form.html', {})
+    comment_form = CommentForm({'name': 'Guillermo', 'url': 'https://open-bootcamp.com', 'comment': 'Comentario'})
+    return render(request, 'form.html', {'comment_form': comment_form})
 
+def goal(request):
+    if request.method !='POST':
+        return HttpResponse("Método no permitido")
+
+    return HttpResponse(request.POST['name'])    
