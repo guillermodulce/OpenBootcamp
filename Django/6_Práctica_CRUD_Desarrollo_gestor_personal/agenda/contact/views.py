6ce17834009da42552b7b0319d80236dbccadfb3
Guillermo Dulce <guillermodulce@gmail.com>
2022-11-14 21:56:10 -0300

    Práctica: Buscador y listas 2

--- Django/6_Práctica_CRUD_Desarrollo_gestor_personal/agenda/contact/views.py
+++ Django/6_Práctica_CRUD_Desarrollo_gestor_personal/agenda/contact/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(name__contains=request.GET.get('search', ''))
+
     context = {
         'contacts': contacts
     }
