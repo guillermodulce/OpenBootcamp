8cb817f15f599563d90bdcae44785e529f98dc4b
Guillermo Dulce <guillermodulce@gmail.com>
2022-12-14 19:21:56 -0300

    Utilidades y repaso

--- Django/7_Utilidades/highlights/highlights/settings.py
+++ Django/7_Utilidades/highlights/highlights/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'optional_parameters',
+    'first_letter',
 ]
 
 MIDDLEWARE = [
--- Django/7_Utilidades/highlights/highlights/urls.py
+++ Django/7_Utilidades/highlights/highlights/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('messages/', include('messages.urls')),
-    path('optional/', include('optional_parameters.urls'))
+    path('optional/', include('optional_parameters.urls')),
+    path('first/', include('first_letter.urls')),
 ]
